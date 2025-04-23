585d019e3ebb7c574c8c2e91a458717ee1ec415a
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-04-23 21:16:18 +1000

    test: added post route test case

--- tests/items.test.js
+++ tests/items.test.js
@@ -7,9 +7,21 @@
 
 // Importing a testing helper function from supertest
 const request = require('supertest');
+const mongoose = require('mongoose');
+const connectDB = require("../src/utils/database");
 
 // Importing the code that we want to test
 const app = require("../src/index");
+const { describe } = require('yargs');
+const { default: test } = require('node:test');
+
+beforeAll(async () => {
+    await connectDB();
+});
+
+afterAll(async () => {
+    await mongoose.connection.close();
+});
 
 describe('GET /health', () => {
     it('should return status OK', async() => {
@@ -17,5 +29,28 @@ describe('GET /health', () => {
         .get('/health');
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({ status: "OK" });
+    });
+});
+
+describe('POST /items', () => {
+    test('should create a new item and return it', async () => {
+        const newItem = {
+            name: "Test Monitor",
+            price: 29.99,
+            quantityInStock: 25,
+            category: "Accessories"
+        };
+
+        const response = await request(app)
+        .post('/items')
+        .send(newItem)
+        .expect(201);
+
+        expect(response.body).toMatchObject({
+            name: newItem.name,
+            price: newItem.price,
+            quantityInStock: newItem.quantityInStock,
+            category: newItem.category
+        });
     });
 });
